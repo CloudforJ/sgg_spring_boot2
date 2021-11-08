bc175f1e22e9cd57132cafaf104ac6788c4c94bb
CloudforJ <bowenlu.cloud@gmail.com>
2021-11-08 16:19:17 +0800

    start from hello world

---
+++ boot-01-helloworld/src/main/java/com/atguigu/boot/MainApplication.java
@@ -0,0 +1,20 @@
+package com.atguigu.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class MainApplication {
+
+    public static void main(String[] args) {
+        //  返回IOC容器
+        ConfigurableApplicationContext run = SpringApplication.run(MainApplication.class, args);
+
+
+        String[] names = run.getBeanDefinitionNames();
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
