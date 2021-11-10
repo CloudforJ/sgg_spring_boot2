d66bb063d6755e45982d33b2ba804b4b81815ce5
CloudforJ <bowenlu.cloud@gmail.com>
2021-11-10 10:35:08 +0800

    Pet and Person

--- boot-02-helloworld/src/main/java/com/atguigu/boot/Boot02HelloworldApplication.java
+++ boot-02-helloworld/src/main/java/com/atguigu/boot/Boot02HelloworldApplication.java
@@ -1,4 +1,4 @@
-package com.atguigu.boot02helloworld;
+package com.atguigu.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
