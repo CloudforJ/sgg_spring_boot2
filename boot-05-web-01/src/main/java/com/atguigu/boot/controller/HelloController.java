a5bfe7e5a5be4273275440f454d2a243809106fe
CloudforJ <bowenlu.cloud@gmail.com>
2021-11-12 17:16:25 +0800

    Update HelloController.java

--- boot-05-web-01/src/main/java/com/atguigu/boot/controller/HelloController.java
+++ boot-05-web-01/src/main/java/com/atguigu/boot/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.atguigu.boot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,26 @@ public class HelloController {
     @RequestMapping("/yukirin.jpg")
     public String hello() {
         return "aaaa";
+    }
+
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public String getUser() {
+        return "GET-张三";
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public String savaUser() {
+        return "POST-张三";
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.PUT)
+    public String putUser() {
+        return "PUT-张三";
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.DELETE)
+    public String deleteUser() {
+        return "DELETE-张三";
     }
 }
